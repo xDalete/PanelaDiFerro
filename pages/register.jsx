9db9b2fe7d4b02751fbbcb898722b5c1afa99a91
Patrick Ferreira Carvalho <patrick651265@gmail.com>
2022-11-07 23:40:17 -0300

    form data validation

--- pages/register.jsx
+++ pages/register.jsx
@@ -1,7 +1,50 @@
+import { useRef } from 'react';
 import { Card, CardHeader, CardTitle, CardBody, Form, FormGroup, Input, Label } from 'reactstrap';
 import styles from '../styles/pages/Register.module.scss';
+const axios = require('axios').default;
 
 export default function MyApp() {
+    let [disabled, setDisabled] = useState(false)
+    let title = useRef("");
+    let preparingTime = useRef(0);
+    let portions = useRef(0);
+    let ingredients = useRef("");
+    let preparationMode = useref("");
+    let observations = useRef("");
+    let images = useRef(null);
+    const handleSubmit = (event) => {
+        if (title.current.length < 4) return
+        if (ingredients.current.split("\n").length <= 2) return
+        if (preparationMode.current.length < 50) return
+        if (images.current === null) {
+            return
+        } else {
+            const config = {
+                headers: { 'content-type': 'multipart/form-data' },
+                onUploadProgress: (event) => {
+                  setProgress(Math.round((event.loaded * 100) / event.total))
+                },
+              };
+              const formData = new FormData();
+              formData.append("thumb", images)
+              formData.append("titulo", title)
+              formData.append("ingredientes", ingredients.trim().split("\n"))
+              formData.append("modo_preparo", preparationMode)
+              formData.append("observacoes", observations)
+              formData.append("tempo_preparo", preparingTime)
+              formData.append("porcoes", portions)
+        
+              axios.post('/api/register', formData, config).then(response => {
+                setDisabled(true)
+                setId(response.data.id)
+                setErro(``)
+              })
+                .catch(err => {
+                  (err == "AxiosError: Request failed with status code 400") ? setErro(`Alguma informação não foi preenchida.`) : null
+                })
+        }
+    }
+
     return (
         <section className='container'>
             <Card className="p-4 shadow">
@@ -17,7 +60,7 @@ export default function MyApp() {
                                         <Label className={`me-2 ${styles.titlesColor}`}>Título</Label>
                                         <i className={`medium material-icons ${styles.themeColor}`}>title</i>
                                     </div>
-                                    <Input type="text" placeholder="Ex.: Café com Creme de Avelã"></Input>
+                                    <Input disabled={disabled} type="text" name="titulo" placeholder="Ex.: Café com Creme de Avelã" ref={title} required></Input>
                                 </FormGroup>
                                 <div className='row'>
                                     <FormGroup style={{ width: "50%" }}>
@@ -25,14 +68,14 @@ export default function MyApp() {
                                             <Label className={`me-2 ${styles.titlesColor}`}>Tempo de Preparo</Label>
                                             <i className={`medium material-icons ${styles.themeColor}`}>access_alarm</i>
                                         </div>
-                                        <Input type="number" placeholder="Tempo em minutos"></Input>
+                                        <Input  disabled={disabled} type="number" name="tempo_preparo" placeholder="Tempo em minutos" ref={preparingTime} required></Input>
                                     </FormGroup>
                                     <FormGroup style={{ width: "50%" }}>
                                         <div className='my-0 d-flex justify-content-start'>
                                             <Label className={`me-2 ${styles.titlesColor}`}>Porções</Label>
                                             <i className={`medium material-icons ${styles.themeColor}`}>free_breakfast</i>
                                         </div>
-                                        <Input type="number" placeholder="Número de porções"></Input>
+                                        <Input  disabled={disabled} type="number" name="porcoes" placeholder="Número de porções" ref={portions} required></Input>
                                     </FormGroup>
                                 </div>
                             </div>
@@ -42,11 +85,7 @@ export default function MyApp() {
                                         <Label className={`me-2 ${styles.titlesColor}`}>Ingredientes</Label>
                                         <i className={`medium material-icons ${styles.themeColor}`}>format_list_bulleted</i>
                                     </div>
-<<<<<<< HEAD
-                                    <Input type="textarea" rows="5" placeholder={`Insira os ingredientes separados por linha. Ex.:\n2 colheres de sal\n4 colheres de sopa de pó royal\n1 lata de milho`}></Input>
-=======
-                                    <Input type="textarea" rows="5" placeholder={`Insira os ingredientes separados por linhas. Ex.:\n2 colheres de sal\n4 colheres de sopa de pó royal\n1 lata de milho`}></Input>
->>>>>>> f620831876b7694766c598d01f1d3be118d634fb
+                                    <Input  disabled={disabled} type="textarea" name="ingredientes" rows="5" placeholder={`Insira os ingredientes separados por linhas. Ex.:\n2 colheres de sal\n4 colheres de sopa de pó royal\n1 lata de milho`} ref={ingredients} required></Input>
                                 </FormGroup>
                             </div>
                         </div>
@@ -57,7 +96,7 @@ export default function MyApp() {
                                         <Label className={`me-2 ${styles.titlesColor}`}>Modo de Preparo</Label>
                                         <i className={`medium material-icons ${styles.themeColor}`}>cake</i>
                                     </div>
-                                    <Input type="textarea" rows="5" placeholder="Descrição da receita"></Input>
+                                    <Input  disabled={disabled} type="textarea" name="modo_preparo" rows="5" placeholder="Descrição da receita" ref={preparationMode} required></Input>
                                 </FormGroup>
                             </div>
                             <div className='col-md-6'>
@@ -66,7 +105,7 @@ export default function MyApp() {
                                         <Label className={`me-2 ${styles.titlesColor}`}>Observações</Label>
                                         <i className={`medium material-icons ${styles.themeColor}`}>description</i>
                                     </div>
-                                    <Input type="textarea" rows="5" placeholder="Informações adicionais *(opcional)"></Input>
+                                    <Input  disabled={disabled} type="textarea" name="observacoes" rows="5" placeholder="Informações adicionais *(opcional)" ref={observations}></Input>
                                 </FormGroup>
                             </div>
                         </div>
@@ -76,10 +115,10 @@ export default function MyApp() {
                                     <Label className={`me-2 ${styles.titlesColor}`}>Imagem</Label>
                                     <i className={`medium material-icons ${styles.themeColor}`}>image</i>
                                 </div>
-                                <Input type="file"></Input>
+                                <Input  disabled={disabled} type="file" name="imagem" ref={images} required></Input>
                             </div>
                             <div className='col-md-2' style={{ marginTop: "2em" }}>
-                                <button className='btn btn-success w-100'>Salvar</button>
+                                <button type="submit" className='btn btn-success w-100' onClick={handleSubmit()}>Salvar</button>
                             </div>
                             <div className='col-md-4' style={{ marginTop: "2.5em" }}>
                                 <div className="border border-dark rounded" style={{ width: "300px", height: "25px" }}>
