567ee11a9ed4e1a1bea0f72ef70913589edb6959
Dalete <76188189+xDalete@users.noreply.github.com>
2022-11-10 21:41:50 +0000

    é

--- Components/BannerReceitaSearch.jsx
+++ Components/BannerReceitaSearch.jsx
@@ -2,24 +2,24 @@ import styles from '../styles/components/BannerReceitaSearch.module.scss';
 import { showPrepareTimeProps } from '../pages/receita/[id].jsx'
 
 function Receita({ image, titulo, tempo_preparo, porcoes, id }) {
-    return (
-        <a href={`/receita/${id}`}>
-            <div className={styles.BannerReceitaSearch}>
-                <img className={styles.image} src={`${image.replace("upload/","upload/w_240/")}`}/>
-                <p className={styles.titulo}>{titulo}</p>
-                <div style={{ display: "flex", flexDirection: "row" }}>
-                    <div style={{ display: "flex", flexDirection: "row", color: "rgb(255, 136, 0)", marginInline: "4em" }}>
-                        <i className='medium material-icons me-3'>access_alarm</i>
-                        <p>{showPrepareTimeProps(tempo_preparo)}</p>
-                    </div>
-                    <div style={{ display: "flex", flexDirection: "row", color: "rgb(255, 136, 0)" }}>
-                        <i className='medium material-icons me-3'>free_breakfast</i>
-                        <p>{porcoes} porções</p>
-                    </div>
-                </div>
-            </div>
-        </a>
-    );
+  return (
+    <a href={`/receita/${id}`} style={{textDecoration: "none"}}>
+      <div className={styles.BannerReceitaSearch}>
+        <img className={styles.image} src={`${image.replace("upload/", "upload/w_240/")}`} />
+        <p className={styles.titulo}>{titulo}</p>
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          <div style={{ display: "flex", flexDirection: "row", color: "rgb(255, 136, 0)", marginInline: "4em" }}>
+            <i className='medium material-icons me-3'>access_alarm</i>
+            <p>{showPrepareTimeProps(tempo_preparo)}</p>
+          </div>
+          <div style={{ display: "flex", flexDirection: "row", color: "rgb(255, 136, 0)" }}>
+            <i className='medium material-icons me-3'>free_breakfast</i>
+            <p>{porcoes} porções</p>
+          </div>
+        </div>
+      </div>
+    </a>
+  );
 }
 
 export default Receita
